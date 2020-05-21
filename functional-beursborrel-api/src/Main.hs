236866d16b027528b45b02cf450d9abf872335a3
Wouter Doeland <git@wouterdoeland.nl>
2020-05-21 19:56:58 +0200

    Fixed CORS problems

--- functional-beursborrel-api/src/Main.hs
+++ functional-beursborrel-api/src/Main.hs
@@ -17,7 +17,9 @@
 import Web.Spock
 import Web.Spock.Config
 import Network.HTTP.Types.Status
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai                       (Middleware)
+import Network.Wai.Middleware.AddHeaders (addHeaders)
+import Network.Wai.Middleware.Cors       (CorsResourcePolicy(..), cors)
 
 -- Text and JSON data
 import Data.Aeson hiding (json)
@@ -95,7 +97,8 @@ main = do
 
 app :: SpockM SqlBackend () () ()
 app = do
-    middleware simpleCors
+    middleware allowCsrf
+    middleware corsified
     -- drinks
     get "drink" $ do
         -- Return all drinks
@@ -116,7 +119,7 @@ app = do
         -- TODO Fetch default and minprice
         let price = calculatePrice TimeDiffBasedPrice 0.50 0.50 boughtDrinks currTime
         json $ object ["price" .= price]
-    put "admin/order" $ do
+    post "admin/order" $ do
         -- Place an order for drinks
         maybeOrder <- jsonBody :: ApiAction (Maybe Order)
         case maybeOrder of
@@ -171,3 +174,32 @@ errorJson code message =
 
 jsonErrorHandler :: Status -> ActionCtxT ctx IO ()
 jsonErrorHandler (Status code message) = errorJson code (decodeUtf8 message)
+
+
+-- Copied from https://stackoverflow.com/questions/41399055/haskell-yesod-cors-problems-with-browsers-options-requests-when-doing-post-req
+-- | @x-csrf-token@ allowance.
+-- The following header will be set: @Access-Control-Allow-Headers: x-csrf-token@.
+allowCsrf :: Middleware
+allowCsrf = addHeaders [("Access-Control-Allow-Headers", "x-csrf-token,authorization")]
+
+-- | CORS middleware configured with 'appCorsResourcePolicy'.
+corsified :: Middleware
+corsified = cors (const $ Just appCorsResourcePolicy)
+
+-- | Cors resource policy to be used with 'corsified' middleware.
+--
+-- This policy will set the following:
+--
+-- * RequestHeaders: @Content-Type@
+-- * MethodsAllowed: @OPTIONS, GET, PUT, POST@
+appCorsResourcePolicy :: CorsResourcePolicy
+appCorsResourcePolicy = CorsResourcePolicy {
+    corsOrigins        = Nothing
+  , corsMethods        = ["OPTIONS", "GET", "PUT", "POST"]
+  , corsRequestHeaders = ["Authorization", "Content-Type"]
+  , corsExposedHeaders = Nothing
+  , corsMaxAge         = Nothing
+  , corsVaryOrigin     = False
+  , corsRequireOrigin  = False
+  , corsIgnoreFailures = False
+}
